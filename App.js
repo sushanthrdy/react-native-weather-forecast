1a326c3c1b8ca4618b41eaa4840ed86897cab4eb
Sushanth Reddy Daram <sushanth.reddy@hidubai.com>
2024-09-22 11:36:12 +0400

    Handled user location fetching

--- App.js
+++ App.js
@@ -4,8 +4,33 @@ import { s } from "./App.style";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import Home from "./pages/home/Home";
 import backgroundImage from "./assets/background.png";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from "expo-location";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [coordinates, setCoordinates] = useState();
+  useEffect(() => {
+    getUserCoordinates();
+  }, []);
+
+  async function getUserCoordinates() {
+    const { status } = await requestForegroundPermissionsAsync();
+    if (status === "granted") {
+      const location = await getCurrentPositionAsync();
+      setCoordinates({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      });
+    } else {
+      setCoordinates({ lat: "48.85", lng: "2.35" });
+    }
+  }
+
+  console.log(coordinates);
+
   return (
     <ImageBackground
       imageStyle={s.img}
